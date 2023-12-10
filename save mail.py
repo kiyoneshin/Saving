dcfcbf30443fcddce11a4352eca6fb35b0269d42
Nguyễn Kiên <153348429+kiyoneshin@users.noreply.github.com>
2023-12-10 14:03:15 +0700

    Update save mail.py

--- save mail.py
+++ save mail.py
@@ -2,8 +2,6 @@ import socket
 import os
 import base64
 import ssl
-from email import policy
-from email.parser import BytesParser
 
 # Các biến cấu hình
 POP3_SERVER = "pop.example.com"
@@ -40,34 +38,61 @@ def download_email(uid, client_socket):
 
     return email_content
 
-def process_multipart(mime_header, mime_body):
-    #Thực hiện xử lý cho các phần multipart
-    msg = BytesParser(policy=policy.default).parsebytes(mime_body.encode())
-    for part in msg.iter_parts():
-        content_type = part.get_content_type()
-        if content_type.startswith("text/"):
-            process_text_part(part)
-        elif content_type.startswith("image/") or content_type.startswith("application/"):
-            process_attachment(part)
+def save_attachment(attachment_data, filename):
+    with open(filename, 'wb') as attachment_file:
+        attachment_file.write(base64.b64decode(attachment_data))
 
-def process_text_plain(mime_body):
-    #Thực hiện xử lý cho phần text/plain
-    print("Text/Plain Content:")
-    print(mime_body)
+def process_multipart(multipart_data, uid):
+    boundary = multipart_data.get_content_type().split("=")[-1]
+    parts = multipart_data.get_payload()
 
-def process_text_part(part):
+    for part in parts:
+        process_part(part, uid)
+
+def process_part(part, uid):
     content_type = part.get_content_type()
-    content = part.get_payload(decode=True).decode(part.get_content_charset())
-    print(f"Text Part ({content_type}):")
-    print(content)
+    content_disposition = part.get("Content-Disposition")
 
-def process_attachment(part):
-    filename = part.get_filename()
-    if filename:
-        print(f"Found attachment: {filename}")
-        # Lưu tệp đính kèm xuống máy cục bộ của client
-        with open(filename, 'wb') as attachment_file:
-            attachment_file.write(part.get_payload(decode=True))
+    if content_type.startswith("text"):
+        # Xử lý phần văn bản
+        content = part.get_payload(decode=True).decode('utf-8')
+        print(f"Text content:\n{content}")
+    elif content_disposition and content_disposition.startswith("attachment"):
+        # Xử lý phần đính kèm
+        filename = part.get_filename()
+        if filename:
+            filename = f'{uid}_{filename}'
+            save_attachment(part.get_payload(), filename)
+            print(f"Saved attachment: {filename}")
+    elif content_type.startswith("multipart"):
+        # Xử lý phần multipart (đệ quy)
+        process_multipart(part, uid)
+
+def process_mime(email_content, uid):
+    lines = email_content.split('\r\n')
+
+    # Tìm dòng bắt đầu của phần MIME
+    start_index = None
+    for i, line in enumerate(lines):
+        if line.startswith("--"):
+            start_index = i
+            break
+
+    if start_index is not None:
+        # Xử lý từ dòng bắt đầu của phần MIME
+        mime_data = '\r\n'.join(lines[start_index:])
+        msg_start = email_content.find(mime_data)
+        mime_msg = email_content[msg_start:]
+
+        # Phân tích MIME message
+        message = email.message_from_string(mime_msg)
+
+        # Xử lý từng phần trong message
+        for part in message.walk():
+            process_part(part, uid)
+    else:
+        # Nếu không tìm thấy dòng bắt đầu, xử lý nội dung trực tiếp
+        process_part(email.message_from_string(email_content), uid)
 
 def main():
     # Kết nối đến Mail Server qua SSL
@@ -98,17 +123,10 @@ def main():
         uid = uid_response.split()[0]
         if uid not in downloaded_uids:
             email_content = download_email(uid, client_socket)
-            
-            # Xử lý MIME để trích xuất nội dung và đính kèm
-            mime_header, mime_body = extract_mime_parts(email_content)
 
-            # Thực hiện xử lý MIME cho từng loại MIME
-            if mime_header and mime_body:
-                if "multipart" in mime_header.lower():
-                    process_multipart(mime_header, mime_body)
-                elif "text/plain" in mime_header.lower():
-                    process_text_plain(mime_body)
-            
+            # Xử lý để trích xuất nội dung và đính kèm
+            process_mime(email_content, uid)
+
             # Ghi nội dung email vào tệp
             file_path = os.path.join(DOWNLOAD_PATH, f'email_{uid}.txt')
             with open(file_path, 'w', encoding='utf-8') as email_file:
